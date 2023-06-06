c5041b6ecd8e82508b88e3f684ad25aa77ea16a7
wintertime7 <sgwa2001@gmail.com>
2023-06-06 11:12:24 +0000

    home page deveoped

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
 // Main imports
-import React from 'react'
+import React, { useState } from 'react'
+
+// Style imports
+import '../style/Home.css'
 
 function Home() {
+
+  const [textColor, setTextColor] = useState('#fffcf2')
+  const [fontStyle, setFontStyle] = useState('normal')
+  const [isWhite, setIsWhite] = useState(true)
+
+  const handleColorChange = (e) => {
+    setIsWhite(!isWhite)
+    setTextColor(isWhite ? '#e63946' : '#fffcf2')
+    setFontStyle(isWhite ? 'italic' : 'normal')
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <div className="body-area">
+        <div className="left">
+          <p className='left-text'>Hi! My name is</p>
+          <h2>Rainers <span style={{ color:textColor, fontStyle:fontStyle}} className='bodySurnameSpan'
+            onClick={handleColorChange}>Smolenskis</span></h2>
+          <p id='psParagraph'>P.S. Click on my surname ^</p>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
